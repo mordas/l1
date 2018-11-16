2c6f0d4624fb1636017edcdfc982118c96acc3a1
Dmitry <dontbeatmybrin@gmail.com>
2018-11-16 08:28:10 +0300

    first commit

---
+++ JS.js
@@ -0,0 +1,19 @@
+let money = prompt('Ваш бюджет на месяц?', 0);
+let time = prompt('Введите дату в формате YYYY-MM-DD', 0);
+let a = prompt('Введите обязательную статью расходов в этом месяце');
+let b = prompt('Во сколько обойдется?');
+let c = prompt('Введите обязательную статью расходов в этом месяце');
+let d = prompt('Во сколько обойдется?');
+let appData = {
+    budjet: money,
+    date: time,
+    expenses: {a: b,
+        c:d
+    },
+    optionalExpenses: {
+
+    },
+    income: [],
+    savings: false
+}
+alert("Ваш бюджет на сегодня: " + money/30);
